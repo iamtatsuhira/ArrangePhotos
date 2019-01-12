8737483f0512a14276fbf6009aa74e4892b9d4d1
Tatsumasa Hiratsuka <tatsumasa@TatsuMac.local>
2019-01-13 00:52:14 +0900

    ひとまず完成

---
+++ README.md
@@ -0,0 +1,30 @@
+# arrangephotos.pyとは
+`hoge.JPG`みたいな`jpg`ファイルと、`hoge.*`みたいな拡張子以外は同じ名前のファイルを、`hoge.JPG`のExifから得られる日付の情報をもとに作成されたディレクトリに移動させるコード。
+
+# 使い方
+## python仮想環境の構築、必要なライブラリのインストール
+Macの`Homebrew`で入れた`python3`を使用し、そしてシェルは`fish`である場合。
+
+```sh
+$ python3 -m venv myenv
+# activate
+$ source ./myenv/bin/activate.fish
+$ pip install -r requirements.txt
+```
+
+## コードの使い方
+
+```sh
+$ python arrangephotos.py hoge.JPG
+```
+
+これでexif情報を元に`2019-01-13`みたいなディレクトリに`hoge.*`が移動される。
+
+## ディレクトリ内の全てのファイルに対して適用する
+`fish`の場合
+```sh
+$ for i in (ls):
+    python arrangephotos.py $i
+    end
+```
+
--- arrangephotos.py
+++ arrangephotos.py
@@ -2,10 +2,10 @@ from PIL import Image
 from PIL.ExifTags import TAGS
 import os
 import sys
+import shutil
+import glob
 
 ALLAWED_EXTENSIONS = set(['jpg', 'JPG', 'png', 'PNG'])
-
-filename = sys.argv[1]
 
 def allowed_file(filename):
     # eg. filaneme = hoge.txt
@@ -23,17 +23,34 @@ def get_date_from_exif(file):
         exif_table[tag] = value
     
     date_time = exif_table.get("DateTimeOriginal")
+    # 'yyyy:mm:dd hh:mm:ss'というフォーマットなので
+    # split(" ")により" "で分割し、最初の要素（日付に関する部分）のみ取得
     date = date_time.split(" ")[0]
+    # ディレクトリ作成用にフォーマットを'yyyy:mm:dd'から'yyyy-mm-dd'に変更
     date = "-".join(date.split(":"))
     
     return date
 
 def main():
     filename = sys.argv[1]
+
+    # ファイルが存在し、かつ拡張子が指定のものなら
     if filename and allowed_file(filename):
+        # 日付の情報を取得
         date_str = get_date_from_exif(filename)
-        print(date_str)
+        
+        # 日付のディレクトリがないなら
+        if not os.path.isdir(date_str):
+            # ディレクトリを作成
+            os.mkdir(date_str)
+        
+        # 移動するファイル群を取得
+        # 'hoge.JPG'や'hoge.ARW'、つまり'hoge.*'を取得
+        files_to_be_moved = glob.glob(filename.split('.')[0] + ".*")
 
-
+        # ファイル群を日付の名前がついたディレクトリに移動
+        for f in files_to_be_moved:
+            shutil.move(f, date_str)
+        
 if __name__ == "__main__":
     main()
